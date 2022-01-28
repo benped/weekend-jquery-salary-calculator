c917648694cb410d745ef7961fa1d830db6b942a
Ben Pedrick <bpedrick@gmail.com>
2022-01-28 12:00:33 -0600

    Successfully adds input data to the DOM

--- client.js
+++ client.js
@@ -7,6 +7,8 @@ function readyNow(){
 }
 
 function intakeInfo(){
+    
+    // PUT ALL INPUTS INTO AN employee OBJECT
     let employee = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
@@ -14,6 +16,22 @@ function intakeInfo(){
         title: $('#title').val(),
         salary: $('#salary').val(),
     }
- 
+    // CLEAR INPUTS 
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#ID').val('');
+    $('#title').val('');
+    $('#salary').val('');
+        // End clear Inputs
+
+    // Append Data to the DOM
+    $('#salaryTable').append(`<tr><td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.ID}</td>
+    <td>${employee.title}</td>
+    <td>${employee.salary}</td></tr>`)
+        // End table data appended to DOM
+
+
     console.log(employee);
 }
